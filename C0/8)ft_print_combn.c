c79342156162139d31b5e86d9b5729b40e06e821
Nicolas <nicolasglumi@gmail.com>
2022-05-26 20:14:43 +0200

    Update 8)ft_print_combn.c

--- C0/8)ft_print_combn.c
+++ C0/8)ft_print_combn.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
-void ft_fun_begin_here(int totalNumber, int start)
+void ft_fun_begin_here(int totalNumber)
 {
-	char text[11];
+	char text[12];
 	int	index = 1;
 	*text = '0';
-	text[0] = start + '0';
 	
 	while (index < totalNumber)
 	{
-		text[index] = text[index - 1] + 1;
+		text[index] = '0' + index;
 		index++;
 	}
 	text[totalNumber] = ',';
 	text[totalNumber + 1] = ' ';
 	
 	write(1, text, totalNumber+2);
-	
-	int lastNumber = index;
-	int actualIndex = index;
-	
-	while(actualIndex-1 > 0)
+  
+   while (text[0] != 58 - totalNumber)
+   {
+    text[index-1] += 1;
+	if (text[index-1] >= '0'+10) 
 	{
-	    while (lastNumber < 10)
-	    {
-	        text[actualIndex-1] = lastNumber + '0';
-    	    write(1, text, totalNumber+2);
-    	    lastNumber++;
-	    }
-	    lastNumber = text[actualIndex-2] + 2;
-	    text[actualIndex-2] = text[actualIndex-2] + 1;
+	    text[index-2] += 1;
+	    text[index-1] = text[index - 2] + 1;
+	    ft_verify_max(text, index);
 	}
+	write(1, text, totalNumber+2);
+   }
 }
 
-
-
+void ft_verify_max(char *text, int index)
+{
+    if (text[index] == '0'+10) text[index-1] = text[index-2]+1;
+    if (index != 0) ft_verify_max(text, index-1);
+    else text[0] += 1;
+}
 
 void ft_print_combn(int n) 
 {
-    if (n > 1 && n < 10)  ft_fun_begin_here(n,0);
+    if (n > 1 && n < 10)  ft_fun_begin_here(n);
     else if (n < 1) printf("Error, number too much small");
     else if (n > 9) printf("Error, number too much big");
 }
 
 int main()
 {
-    ft_print_combn(3);
+    ft_print_combn(2);
   return 0;
 }
